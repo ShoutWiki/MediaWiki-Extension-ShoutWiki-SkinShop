4a09fc2a0da2df9c752372c5b04aeab784b9e10f
lcawte <lewis@lewiscawte.info>
2012-08-24 19:20:44 +0100

    Message author (en)

--- SkinShop.i18n.php
+++ SkinShop.i18n.php
@@ -8,6 +8,9 @@
 
 $messages = array();
 
+/** English (English)
+ * @author Lcawte
+ */
 $messages['en'] = array(
 		'skinshop'			=> 'Skin Shop',
 		'skinshop-desc'     => 'Adds a [[Special:SkinShop|special page]] allow users to buy premium skins for their wikis',
